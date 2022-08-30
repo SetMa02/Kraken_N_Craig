98da8e0a65ca2bc977454660f362c5ef2117a71e
Максим Майнагашев <maxuser99@mail.ru>
2022-08-30 22:38:05 +0700

    Some Ui updated

--- KrakenNCraig/Assets/Scripts/Player/PlayerScore.cs
+++ KrakenNCraig/Assets/Scripts/Player/PlayerScore.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 using Random = System.Random;
 
@@ -13,6 +14,7 @@ namespace DefaultNamespace
         public  UnityAction StepReached;
         public float Score => _score;
 
+        [SerializeField]private Text _scoreText;
         private Player _player;
         private float _currentScore;
         private float _score = 0;
@@ -42,6 +44,7 @@ namespace DefaultNamespace
             if (_currentScore > Score)
             {
                 _score = _currentScore;
+                _scoreText.text = _score.ToString("0000");
             }
         }
 
